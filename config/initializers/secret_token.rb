63f0a8af63532bad42537e7dfc191e3cfc2cece8
Devishot <sattar.coder@gmail.com>
2013-01-09 20:09:02 +0600

    Rails was created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabbit::Application.config.secret_token = '251db85cff5511a3b3ef5079c2f7459f5321354bb57c4d84a566b539aaf848ed28354bfc6560f92287ca8f4a1764ebb5f208f5a1ec11dd5eb9a1ac1b9cef40e1'
